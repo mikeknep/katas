8ed9563084d0f4eee3e063a9a43a0c667d9de8f6
Mike Knepper <michaelrknepper@gmail.com>
2014-03-27 16:05:24 -0500

    Creates fizzbuzz clojure kata

---
+++ clojure/fizzbuzz/spec/fizzbuzz/core_spec.clj
@@ -0,0 +1,7 @@
+(ns fizzbuzz.core-spec
+  (:require [speclj.core :refer :all]
+            [fizzbuzz.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
---
+++ clojure/fizzbuzz/src/fizzbuzz/core.clj
@@ -0,0 +1,1 @@
+(ns fizzbuzz.core)
