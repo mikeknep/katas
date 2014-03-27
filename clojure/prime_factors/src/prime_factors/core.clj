576a2ff890df4635378cee6ef974929937c08ed9
Mike Knepper <michaelrknepper@gmail.com>
2014-03-27 12:19:22 -0500

    Completes prime factors kata in clojure

--- clojure/prime_factors/src/prime_factors/core.clj
+++ clojure/prime_factors/src/prime_factors/core.clj
@@ -1,1 +1,11 @@
 (ns prime_factors.core)
+
+(defn prime_factors_for [n]
+  (loop [n        n
+         factors  []
+         divisor  2]
+    (if (= n 1)
+      factors
+      (if (= 0 (mod n divisor))
+        (recur (/ n divisor) (conj factors divisor) divisor)
+        (recur n factors (inc divisor))))))
