4d1763fae82eb5a7db583995bb2919527f5ac487
Mike Knepper <michaelrknepper@gmail.com>
2014-03-27 10:17:30 -0500

    Refactors coinchanger using loop and recur

--- clojure/coin_changer/src/coin_changer/core.clj
+++ clojure/coin_changer/src/coin_changer/core.clj
@@ -1,15 +1,13 @@
 (ns coin_changer.core)
 
 
-(defn coinchanger[total change] 
-  (if (= total 0)
-    change
-    (if (>= total 25)
-      (coinchanger (- total 25) (conj change :25))
-      (if (>= total 10)
-        (coinchanger (- total 10) (conj change :10))
-        (if (>= total 5)
-          (coinchanger (- total 5) (conj change :5))
-          (if (>= total 1)
-            (coinchanger (- total 1) (conj change :1)))))))
-)
+(defn coinchanger[total]
+  (loop [total  total
+         change []]
+    (if (= total 0)
+      change
+      (cond
+        (>= total 25) (recur (- total 25) (conj change 25))
+        (>= total 10) (recur (- total 10) (conj change 10))
+        (>= total 5) (recur (- total 5) (conj change 5))
+        (>= total 1) (recur (- total 1) (conj change 1))))))
