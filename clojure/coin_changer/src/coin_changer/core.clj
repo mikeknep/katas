7331d83f687e7c11c986471f4119b30dce8e4b50
Mike Knepper <michaelrknepper@gmail.com>
2014-03-27 10:55:32 -0500

    Refactors coins to vector

--- clojure/coin_changer/src/coin_changer/core.clj
+++ clojure/coin_changer/src/coin_changer/core.clj
@@ -3,11 +3,10 @@
 
 (defn coinchanger[total]
   (loop [total  total
-         change []]
+         change []
+         coins  [25 10 5 1]]
     (if (= total 0)
       change
-      (cond
-        (>= total 25) (recur (- total 25) (conj change 25))
-        (>= total 10) (recur (- total 10) (conj change 10))
-        (>= total 5) (recur (- total 5) (conj change 5))
-        (>= total 1) (recur (- total 1) (conj change 1))))))
+      (if (>= total (first coins))
+        (recur (- total (first coins)) (conj change (first coins)) coins)
+        (recur total change (rest coins))))))
