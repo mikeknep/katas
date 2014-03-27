b3d74819935fe8686153b34abc02d6a0681e1610
Mike Knepper <michaelrknepper@gmail.com>
2014-03-27 15:44:01 -0500

    Adds completed roman numerals kata in clojure

--- clojure/roman_numerals/src/roman_numerals/core.clj
+++ clojure/roman_numerals/src/roman_numerals/core.clj
@@ -1,1 +1,19 @@
 (ns roman_numerals.core)
+
+
+(defn largest_arabic_key [translator]
+  (last (sort (keys translator))))
+
+(defn build_roman_string [roman translator]
+  (apply str [roman (get translator (largest_arabic_key translator))]))
+
+
+(defn number_to_roman [arabic]
+  (loop [arabic       arabic
+         roman        ""
+         translator   {1000 "M" 900 "CM" 500 "D" 400 "CD" 100 "C" 90 "XC" 50 "L" 40 "XL" 10 "X" 9 "IX" 5 "V" 4 "IV" 1 "I"}]
+    (if (= 0 arabic)
+      roman
+      (if (>= arabic (largest_arabic_key translator))
+        (recur (- arabic (largest_arabic_key translator)) (build_roman_string roman translator) translator)
+        (recur arabic roman (dissoc translator (largest_arabic_key translator)))))))
